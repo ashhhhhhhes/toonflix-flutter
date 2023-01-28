c7ba898da084f3cbf2e5f59ff0f77573e850e69b
ash <email@example.com>
2023-01-28 16:53:14 +0900

    Add Button

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,11 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xFF181818),
+        backgroundColor: const Color(0xFF181818),
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 40),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const SizedBox(
                 height: 80,
@@ -23,7 +24,7 @@ class App extends StatelessWidget {
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      const Text(
+                      Text(
                         'Hey, Selena',
                         style: TextStyle(
                             color: Colors.white,
@@ -33,9 +34,47 @@ class App extends StatelessWidget {
                       Text(
                         'Welcom back',
                         style: TextStyle(
-                            color: Colors.white.withOpacity(0.8), fontSize: 18),
-                      )
+                            color: Color.fromRGBO(255, 255, 255, 0.8),
+                            fontSize: 18),
+                      ),
                     ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(color: Colors.white.withOpacity(0.8)),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                '\$5 194 482',
+                style: TextStyle(
+                    color: Colors.white.withOpacity(0.8),
+                    fontSize: 44,
+                    fontWeight: FontWeight.w800),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(45)),
+                    child: const Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                      child: Text(
+                        'Transfer',
+                        style: TextStyle(fontSize: 22),
+                      ),
+                    ),
                   )
                 ],
               )
